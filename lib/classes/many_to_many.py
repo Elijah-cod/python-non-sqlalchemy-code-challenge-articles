2140cedbd3c66a74785034ef3a640ffc63a7b15e
Elijah <iamngunjiri@gmail.com>
2025-02-27 06:10:06 +0300

    Complete the author and article tests

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,24 +1,36 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        self._title = title
+        Article.all.append(self)
+    
+    @property
+    def title(self):
+        return self._title  
+
 class Author:
     def __init__(self, name):
-        self.name = name
-
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
-
+        return list({article.magazine for article in self.articles()})
+    
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        topics = {magazine.category for magazine in self.magazines()}
+        return  list(topics) if topics else None
 
 class Magazine:
     def __init__(self, name, category):
@@ -26,7 +38,7 @@ class Magazine:
         self.category = category
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
         pass
